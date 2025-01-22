ab27738f4282d47c1a6cb7525c7c1c564dc2dfb5
Gustavo Teixeira <gteixeiradesousa7@gmail.com>
2025-01-22 10:56:07 -0300

    💄 style: Melhoria na aparência do menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,14 +3,15 @@ extends Node2D
 
 const LEVEL_01: Resource = preload("res://Scenes/level_01.tscn")
 const CURSOR: Resource = preload("res://Assets/PNG/UI/cursor.png")
-@onready var start_button: Button = $CanvasLayer/StartButton
-@onready var quit_button: Button = $CanvasLayer/QuitButton
 @onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
+@onready var play_button: TextureButton = $CanvasLayer/Buttons/PlayButton/TextureButton
+@onready var config_button: TextureButton = $CanvasLayer/Buttons/ConfigButton/TextureButton
+@onready var exit_button: TextureButton = $CanvasLayer/Buttons/ExitButton/TextureButton
 
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(CURSOR)
-	start_button.pressed.connect(_on_start)
-	quit_button.pressed.connect(_on_quit)
+	play_button.pressed.connect(_on_start)
+	exit_button.pressed.connect(_on_quit)
 	enemy_spawn_timer.connect("timeout", _spawn_enemy)
 
 func _physics_process(_delta: float) -> void:
